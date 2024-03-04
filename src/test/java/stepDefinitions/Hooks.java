ee6b4e6b62bd5a3f17de1d87a431bb711934858b
Daniel Phillips <daniel.phillips@ensono.com>
2024-03-04 10:14:06 +0000

    Update session state

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -9,23 +9,43 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 
+/**
+ * Class containing Cucumber hooks for setup and teardown actions.
+ */
 public class Hooks {
 
+    /**
+     * Sets up PlaywrightManager before all scenarios.
+     */
     @BeforeAll
     public static void setup() {
         PlaywrightManager.startPlaywright();
     }
 
+    /**
+     * Tears down PlaywrightManager after all scenarios.
+     */
     @AfterAll
     public static void tearDown() {
         PlaywrightManager.perform().shutdown();
     }
 
+    /**
+     * Launches the test before each scenario.
+     */
     @Before
     public void beforeScenario() {
         PlaywrightManager.perform().launchTest();
     }
 
+    /**
+     * Performs cleanup actions after each scenario.
+     * Will capture and attach a screenshot to reports upon failure.
+     *
+     * If the <b>trace</b> flag is enabled, this will call {@link Hooks#attachTrace(Scenario, String)}.
+     *
+     * @param scenario The scenario that just ran.
+     */
     @After
     public void afterScenario(Scenario scenario) {
         if (scenario.isFailed()) {
@@ -40,6 +60,12 @@ public class Hooks {
         PlaywrightManager.perform().endTest();
     }
 
+    /**
+     * Attaches a trace file to the scenario and creates a link to this within reports.
+     *
+     * @param scenario The scenario to which the trace will be attached.
+     * @param name     The name of the scenario.
+     */
     private void attachTrace(Scenario scenario, String name) {
         Path trace = Paths.get(String.format("target/trace/%s-%s.zip", name, UUID.randomUUID()));
         String linkHtml = String.format(
